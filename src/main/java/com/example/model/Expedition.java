5774bad9fc660dd1dec9a85868b4671d34da2c34
Day Fernadez <dayfernu@hotmail.com>
2026-09-03 15:15:28 -0500

    IasLab

--- src/main/java/com/example/model/Expedition.java
+++ src/main/java/com/example/model/Expedition.java
@@ -1,4 +1,17 @@
 package com.example.model;
 
-public class Expedition {
-}
+public class Sighting {
+
+    private Integer id;
+    private String Name;
+    private String Code;
+    private String Region;
+    private String BaseCamp
+    private String Leader;
+    private String StartDate;
+    private String EndDate;
+    private Integer Estate;
+
+
+
+        
---
+++ src/main/java/com/example/model/Sighting.java
@@ -0,0 +1,17 @@
+package com.example.model;
+
+public class Sighting {
+
+    private Integer id;
+        private String SightingCode;
+        private String Name;
+        private String description;
+        private String ScientificName;
+        private String SightedAt;
+        private String Location;
+        private Integer Quantity;
+        private Integer ConfidenceLevel;
+        private Integer ExpeditionId;
+
+
+
